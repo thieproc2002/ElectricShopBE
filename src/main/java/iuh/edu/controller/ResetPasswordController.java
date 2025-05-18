e294c3bb2fa5604f21ea69659471aafea14bcfd2
Thiep Tran <trantrongthiep2002@gmail.com>
2025-05-18 14:45:31 +0700

    commit

--- src/main/java/iuh/edu/controller/ResetPasswordController.java
+++ src/main/java/iuh/edu/controller/ResetPasswordController.java
@@ -5,17 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import iuh.edu.entity.User;
 import iuh.edu.repository.UserRepository;
 
 @Controller
 @RequestMapping("/forgot-password")
+@CrossOrigin("*")
 public class ResetPasswordController {
 
     @Autowired
